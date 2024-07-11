72897c4fc20adb81f67f1827c9230e3d5be6a065
Daddy4AI <alanqwyuan@gmail.com>
2024-07-11 14:43:00 +0800

    setup

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCmlxG5PBVji_T3dAGJT4L675NNWCyRhqU",
+  authDomain: "beebee-c1ccc.firebaseapp.com",
+  databaseURL: "https://beebee-c1ccc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "beebee-c1ccc",
+  storageBucket: "beebee-c1ccc.appspot.com",
+  messagingSenderId: "409434426338",
+  appId: "1:409434426338:web:88e7cdd52a7fd3d55587bb"
+};
+
+export default firebaseConfig;
